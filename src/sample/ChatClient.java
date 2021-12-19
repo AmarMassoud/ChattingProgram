346ea77898c77744e04ad78b45ead05f13c9ae44
Amar05 <a.walid20010@gmail.com>
2021-12-19 21:12:31 +0300

    chat added

--- src/sample/ChatClient.java
+++ src/sample/ChatClient.java
@@ -34,4 +34,8 @@ public class ChatClient {
         out.close();
         clientSocket.close();
     }
+
+    public Socket getClientSocket() {
+        return clientSocket;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,12 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.awt.datatransfer.Clipboard;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class Main extends Application {
     TextField textField = new TextField();
@@ -18,19 +23,19 @@ public class Main extends Application {
     StackPane mainLayout = new StackPane();
     Button sendButton = new Button("Send");
     Scene mainScene = new Scene(mainLayout, 400, 400);
-
-
+    BufferedReader in;
     @Override
     public void start(Stage primaryStage) throws Exception{
         ChatClient client = new ChatClient();
         client.startConnection("127.0.0.1", 9563);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         window = primaryStage;
-
+        in = new BufferedReader(new InputStreamReader(client.getClientSocket().getInputStream()));
+        chat();
         sendButton.setOnAction(event -> {
             String text = textField.getText();
             try {
-                client.sendMessage(text);
+                System.out.println(client.sendMessage(text));
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -43,10 +48,31 @@ public class Main extends Application {
         sendButton.setTranslateY(50);
         mainLayout.getChildren().add(textField);
         window.show();
-
     }
 
-
+    public void chat(){
+        setTimeout(() -> {
+            try {
+                while(in.ready()){
+                    System.out.println(in.readLine());
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+            chat();
+        }, 500);
+    }
+    public static void setTimeout(Runnable runnable, int delay){
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+                runnable.run();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+        }).start();
+    }
     public static void main(String[] args) {
         launch(args);
     }
